95652855c86e87b9a342792fe0372ae31d99bc84
monk3yd <jegajardog@gmail.com>
2022-12-05 16:52:17 -0300

    Init

---
+++ main.py
@@ -0,0 +1,58 @@
+import sys, pygame
+
+
+def main():
+    pygame.init()
+
+    # display size
+    width, height = 1440, 890
+    size = (width, height)
+
+    # movement speed
+    speed = [1, 1]
+
+    black = 0, 0, 0
+
+    # display (surface)
+    screen = pygame.display.set_mode(size)
+
+    # TODO: load snake image (rectangle)
+    # ball = pygame.image.load("intro_ball.gif")
+
+    color = (255, 0, 0)
+    rect = pygame.Rect(30, 30, 80, 80)
+    snake = pygame.draw.rect(screen, color, rect)
+
+    # get image into rectangle
+    # ballrect = ball.get_rect()
+
+    # FPS
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # TODO: snake collision with own tail
+                # TODO: snake collision with edge of display
+                sys.exit()
+
+    #     ballrect = ballrect.move(speed)
+    #
+    #     # collision within x axis
+    #     if ballrect.left < 0 or ballrect.right > width:
+    #         # direction
+    #         speed[0] = -speed[0]
+    #
+    #     # collision within y axis
+    #     if ballrect.top < 0 or ballrect.bottom > height:
+    #         # direction
+    #         speed[1] = -speed[1]
+    #
+    #     # randomly create food (circles)
+    #     # when head of snake eats food, grow +1
+    #
+        screen.fill(black)
+        screen.blit(screen, snake)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
